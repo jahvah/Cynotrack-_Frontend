e82941a59dabf51fedd297193ec1906611f2a81b
aly-0421 <lysaclvi@gmail.com>
2026-03-12 10:58:51 +0800

    Update GetSpecimenBySelfStorage.php

--- user/StaffUser/StaffSpecimenRequestCrud/StaffSpecimenRequestSelfStorageCrud/GetSpecimenBySelfStorage.php
+++ user/StaffUser/StaffSpecimenRequestCrud/StaffSpecimenRequestSelfStorageCrud/GetSpecimenBySelfStorage.php
@@ -8,22 +8,21 @@ if ($storage_user_id <= 0) {
     exit();
 }
 
-$query = mysqli_query($conn, "
+$query = mysqli_query($conn,"
     SELECT specimen_id, unique_code, quantity
     FROM specimens
-    WHERE specimen_owner_type = 'storage'
-      AND specimen_owner_id = '$storage_user_id'
-      AND status = 'stored'
-      AND quantity > 0
+    WHERE specimen_owner_type='storage'
+      AND specimen_owner_id='$storage_user_id'
+      AND status='stored'
+      AND quantity>0
 ");
 
-if (mysqli_num_rows($query) > 0) {
-    while ($row = mysqli_fetch_assoc($query)) {
-        echo "<option value='{$row['specimen_id']}'>
-                {$row['unique_code']} (Available: {$row['quantity']})
-              </option>";
+if(mysqli_num_rows($query) > 0){
+    echo "<option value=''>-- Select Specimen --</option>";
+    while($row = mysqli_fetch_assoc($query)){
+        echo "<option value='{$row['specimen_id']}'>{$row['unique_code']} (Available: {$row['quantity']})</option>";
     }
-} else {
+}else{
     echo "<option value=''>No specimens available</option>";
 }
 ?>
